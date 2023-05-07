8ee939b0e9dba1a51098e1e44f16c3f443f4e658
Chris Nisbet <cnisbet@digi.com>
2023-05-08 06:06:48 +1200

    replaced gen_parity with a better implementation that just uses bitwise operations.

--- src/dce.h
+++ src/dce.h
@@ -5,19 +5,18 @@
 
 #include <libubox/uloop.h>
 
+#include <stdint.h>
+
 #define DCE_CL_DSR 1
 #define DCE_CL_DCD 2
 #define DCE_CL_CTS 4
 #define DCE_CL_DTR 8
 #define DCE_CL_LE 16
 
-/* This is a cool piece of code found by Chris Osborn (fozztexx@fozztexx.com) from
- * https://graphics.stanford.edu/~seander/bithacks.html#ParityWith64Bits  that
- * computes even parity for any 8 bit data value
- */
-#ifndef gen_parity
-#  define gen_parity(v) ((unsigned char)(((((v) * 0x0101010101010101ULL) & 0x8040201008040201ULL) % 0x1FF) & 1))
-#endif
+static inline unsigned int gen_parity(uint_fast8_t const ch)
+{
+    return (0x6996 >> (((ch >> 4) ^ ch) & 0x0f)) & 1;
+}
 
 /* This is an impressive piece of code written by Chris Osborn (fozztexx@fozztexx.com)
  * that given a value where SPACE=0,ODD=1,EVEN=2,MARK=3 parity is present in p,
